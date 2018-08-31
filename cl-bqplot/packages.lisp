91e1b93635b8a8bc28d0d50de52110f67d6078da
Chris Schafmeister <meister@temple.edu>
2018-08-31 18:09:12 -0400

    Change package names

--- cl-bqplot/packages.lisp
+++ cl-bqplot/packages.lisp
@@ -1,7 +1,9 @@
 (defpackage "BQPLOT"
   (:shadow #:close #:map #:max #:min #:clear)
   (:use :common-lisp
-        :cl-jupyter-widgets)
+        :cl-jupyter
+        :cl-ipywidgets)
+  (:import-from :fredokun-utilities #:[] #:[]-contains)
   (:export
    )
   (:documentation "Implements the bqplot package"))
